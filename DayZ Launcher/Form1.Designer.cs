40f0df5730bff58bc563818dc53113d13dcfebdb
Marcel de Vries <marceldevries@my89.nl>
2012-06-17 03:58:01 +0200

    Initial commit

---
+++ DayZ Launcher/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play DayZ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Run with beta";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(79, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Load DayZ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 143);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DayZ Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+
+
+
+
+
+    }
+}
+
---
+++ DayZ Launcher/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        static string Version = "0.2";
+        
+        string ConfigPath;
+
+        string A2Path;
+        string A2OAPath;
+
+        bool LoadDayZ;
+        bool RunBeta;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text += " v" + Version;
+
+            Application.ApplicationExit += new EventHandler(Application_ApplicationExit);
+
+            LoadDayZ = false;
+            RunBeta = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string value;
+
+            ConfigPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\dayzlauncher.xml";
+
+            if (File.Exists(ConfigPath))
+            {
+                XDocument doc = XDocument.Load(ConfigPath);
+
+                LoadDayZ = Convert.ToBoolean((from xml2 in doc.Descendants("config") select xml2.Element("dayz").Value).FirstOrDefault());
+                RunBeta = Convert.ToBoolean((from xml2 in doc.Descendants("config") select xml2.Element("beta").Value).FirstOrDefault());
+
+                if (LoadDayZ)
+                {
+                    checkBox2.Checked = true;
+                }
+
+                if (RunBeta)
+                {
+                    checkBox1.Checked = true;
+                }
+            }
+
+            // Find ArmA 2 path
+            value = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Bohemia Interactive Studio\ArmA 2", "main", null);
+
+            if (value != null)
+            {
+                A2Path = value;
+            }
+            else
+            {
+                value = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Bohemia Interactive Studio\ArmA 2", "main", null);
+
+                if (value != null)
+                {
+                    A2Path = value;
+                }
+                else
+                {
+                    A2Path = null;
+                }
+            }
+
+            // Find ArmA 2 OA path
+            value = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Bohemia Interactive Studio\ArmA 2 OA", "main", null);
+
+            if (value != null)
+            {
+                A2OAPath = value;
+            }
+            else
+            {
+                value = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Bohemia Interactive Studio\ArmA 2 OA", "main", null);
+
+                if (value != null)
+                {
+                    A2OAPath = value;
+                }
+                else
+                {
+                    A2OAPath = null;
+                }
+            }
+
+            if (A2Path == null || A2OAPath == null)
+            {
+                MessageBox.Show("You haven't installed A2:CO properly!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+
+            if (!File.Exists(A2OAPath + "\\@dayz\\addons\\dayz_code.pbo"))
+            {
+                //MessageBox.Show("You haven't installed DayZ properly!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //this.Close();
+                LoadDayZ = false;
+                checkBox2.Checked = false;
+                checkBox2.Enabled = false;
+                checkBox2.Text += " (Not installed)";
+            }
+
+            if (!File.Exists(A2OAPath + "\\Expansion\\beta\\arma2oa.exe"))
+            {
+                RunBeta = false;
+                checkBox1.Checked = false;
+                checkBox1.Enabled = false;
+                checkBox1.Text += " (Not installed)";
+            }
+            else
+            {
+                FileVersionInfo info = FileVersionInfo.GetVersionInfo(A2OAPath + "\\Expansion\\beta\\arma2oa.exe");
+                checkBox1.Text += " (" + info.ProductVersion + ")";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            if (RunBeta)
+            {
+                startInfo.FileName = A2OAPath + "\\Expansion\\beta\\arma2oa.exe";
+            }
+            else
+            {
+                startInfo.FileName = A2OAPath + "\\arma2oa.exe";
+            }
+
+            startInfo.WorkingDirectory = A2OAPath;
+
+            startInfo.Arguments = "\"-mod=" + A2Path + ";Expansion;ca";
+
+            if (RunBeta)
+            {
+                startInfo.Arguments += ";Expansion\\beta;Expansion\\beta\\Expansion";
+            }
+
+            if (LoadDayZ)
+            {
+                startInfo.Arguments += ";@dayz";
+            }
+
+            startInfo.Arguments += "\" -nosplash -world=empty";
+
+            Process.Start(startInfo);
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                RunBeta = true;
+            }
+            else
+            {
+                RunBeta = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                LoadDayZ = true;
+            }
+            else
+            {
+                LoadDayZ = false;
+            }
+        }
+
+        void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            XDocument doc = new XDocument(
+                new XElement("config",
+                    new XElement("version", Version),
+                    new XElement("dayz", LoadDayZ),
+                    new XElement("beta", RunBeta)));
+
+            doc.Save(ConfigPath);
+        }
+    }
+}
